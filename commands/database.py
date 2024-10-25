7af8f1686b6e3e66eb2a46d87edc873bdae920b2
r2001li <ruiang2001@gmail.com>
2024-10-24 20:34:26 -0400

    slightly unjanked (maybe) the janky database features

--- commands/database.py
+++ commands/database.py
@@ -13,7 +13,7 @@ class Database(commands.Cog):
         self.conn = sqlite3.connect(config.DATABASE_DIR + "/" + config.DATABASE_FILENAME)
 
         cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS tags (guildID integer, tagName text, tagContent text)''')
+        cursor.execute('''CREATE TABLE IF NOT EXISTS tags (guildID integer, tagName text UNIQUE PRIMARY KEY, tagContent text)''')
         self.conn.commit()
         cursor.close()
     
@@ -25,14 +25,22 @@ class Database(commands.Cog):
         self.conn.commit()
         cursor.close()
 
-        await ctx.respond(f"Added tag {tag}.")
+        await ctx.respond(f"Added tag \'{tag}\'.")
     
     @discord.slash_command()
     async def tag_get(self, ctx, tag):
         cursor = self.conn.cursor()
         guildID = ctx.guild.id
         cursor.execute(f"SELECT tagContent FROM tags WHERE guildID = {guildID} AND tagName = \'{tag}\'")
-        content = cursor.fetchone()
+        res = cursor.fetchone()
+
+        if res is None:
+            self.conn.commit()
+            cursor.close()
+            await ctx.respond(f"No matching tag found for \'{tag}\'.")
+            return
+        
+        content = res[0]
         self.conn.commit()
         cursor.close()
 
